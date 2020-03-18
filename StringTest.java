1a762067bd2385aeb375685ad11af9397eddebf2
“mrsliqi <1015913104@qq.com>
2020-03-18 22:29:33 +0800

    0318

---
+++ StringTest.java
@@ -0,0 +1,13 @@
+//引用数据类型
+public class StringTest {
+    public static void main(String[] args) {
+        String a = "HelloWord";
+        System.out.println(a);
+
+        for (int n=0;n<=50;n++){
+            int b = 2 * n;
+            System.out.print(b + " ");
+        }
+
+    }
+}
