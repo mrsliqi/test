804998486b30350c0dd5d75c012e65e939b92405
“mrsliqi <1015913104@qq.com>
2020-04-13 16:34:49 +0800

    111

---
+++ project02/PersonTest.java
@@ -0,0 +1,35 @@
+package project02;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        //创建person类的对象
+        Person p = new Person();
+        //调用对象的结构
+        //调用属性 "对象.属性"
+        p.name = "tom";
+        p.isMale = true;
+        System.out.println(p.name);
+
+        //调用方法  "对象.方法"
+        p.eat();
+        p.sleep();
+        p.talk("Chinese");
+    }
+}
+class Person{
+    //属性 = 成员变量 = field = 域、字段
+    String name;
+    int age;
+    boolean isMale;
+
+    //方法 = 成员方法 = 函数 = method
+    public void eat(){
+        System.out.println("人可以吃饭");
+    }
+    public void sleep(){
+        System.out.println("人可以睡觉");
+    }
+    public void talk(String language){
+        System.out.println("人可以说话：" + language);
+    }
+}
